31c699a8b533305b31bb689c1cf12db0475142ec
ShafaatKhan <shafaatkhan721@gmail.com>
2019-12-23 20:41:52 -0500

    updated build.gradle and added some basic code to main class

--- src/main/java/FPLbot/Main.java
+++ src/main/java/FPLbot/Main.java
@@ -1,7 +1,14 @@
 package FPLbot;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import javax.security.auth.login.LoginException;
 
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws LoginException{
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "NjU4ODM0NTQwMjE0ODc4MjA5.XgFjTA.oMWdIDyzWegs4TUW7DbBtbmhNco";
+        builder.setToken(token);
+        builder.build();
     }
 }
